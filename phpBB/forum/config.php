bf4a0cf98433066b5dfc77cdcb07ebbf1e66fae9
Grigori Kochanov <grigori@grik.net>
2020-05-27 16:39:54 +0300

    Adding base configs for phpBB

---
+++ phpBB/forum/config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'database';
+$dbport = '';
+$dbname = $_SERVER['MYSQL_DATABASE'];
+$dbuser = $_SERVER['MYSQL_USER'];
+$dbpasswd = $_SERVER['MYSQL_PASSWORD'];
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
