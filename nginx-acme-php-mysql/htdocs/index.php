24c3ccab5a83aae715afe78914dd53012c225338
Grigori Kochanov <grigori@grik.net>
2020-06-07 21:54:39 +0300

    Added a DB init dump, writing a PHP sample script

--- nginx-acme-php-mysql/htdocs/index.php
+++ nginx-acme-php-mysql/htdocs/index.php
@@ -1,2 +1,7 @@
 <?php
 
+$db_name = $_SERVER['DB_NAME'];
+$db_user = $_SERVER['DB_USER'];
+$db_password = $_SERVER['DB_PASSWORD'];
+$options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+$pdo = new PDO('mysql:host=database;dbname='.$db_name,$db_user,$db_password,$options);
---
+++ nginx-acme-php-mysql/mysql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE T (
+    id integer unsigned PRIMARY KEY,
+    field integer
+);
+
+INSERT INTO T (`id`, `field`) VALUES (1,100),(2,101);
